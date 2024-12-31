ca1e9ae1118c6ea833425446ba2989cb2e2ada32
hypersonicstorm <73567298+hypersonicstorm@users.noreply.github.com>
2024-12-30 19:38:10 -0700

    Update run_battle.py

--- fp/run_battle.py
+++ fp/run_battle.py
@@ -240,7 +240,7 @@ async def start_battle(ps_websocket_client, pokemon_battle_type):
     else:
         battle = await start_standard_battle(ps_websocket_client, pokemon_battle_type)
 
-    await ps_websocket_client.send_message(battle.battle_tag, ["hf"])
+    await ps_websocket_client.send_message(battle.battle_tag, ["glhf"])
     await ps_websocket_client.send_message(battle.battle_tag, ["/timer on"])
 
     return battle
